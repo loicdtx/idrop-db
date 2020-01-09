37c3da49dcc242dcf6e42f6535439247822b864e
Loïc Dutrieux <loic.dutrieux@gmail.com>
2020-01-09 10:06:56 +0100

    New command to mirror a database to another one

--- idb/scripts/db_init.py
+++ idb/scripts/db_init.py
@@ -13,8 +13,24 @@ Create database tables according to schemas defined in idb/models.py
 Optionally populates species table using records from a csv file
 
 Example:
+    ###########
+    ## Simple examle with default environment
+    ###########
     echo "SAP,sapelli\\nKOS,kossipo" > species.csv
     db_init.py --species species.csv
+
+    ###########
+    ## Create a sqlite database
+    ###########
+
+    # The configuration file must contain a section (e.g. named sqlite) with
+    # drivername=sqlite and database=/path/to/file_database.sqlite
+    # db_init.py will create the database if it does not already exist, but it is
+    # somewhat slow. It is much faster to first create an empty database running
+    # spatialite /path/to/file_database.sqlite and then running the below command
+    # that will take care of setting up the tables and relations
+
+    db_init.py --env sqlite
 """
 
 
