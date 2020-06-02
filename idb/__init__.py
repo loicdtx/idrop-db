5d4b2c553f8d2b9de9e39e5627a1c06a56f18f87
Loïc Dutrieux <loic.dutrieux@gmail.com>
2020-06-02 12:45:43 +0200

    New release

--- idb/__init__.py
+++ idb/__init__.py
@@ -6,7 +6,7 @@ from geoalchemy2 import Geography
 
 from idb.models import Species, Inventory, Interpreted, Studyarea
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 def add_inventories(session, fc):
